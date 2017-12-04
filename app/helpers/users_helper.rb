77a1e2c5a1648f58ba116da9aaf36808f3aa916f
André Laszlo <andre@laszlo.nu>
2017-12-04 23:12:15 +0100

    Change referral id algorithm to make shorter urls

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,18 @@
 module UsersHelper
   def self.unused_referral_code
-    referral_code = SecureRandom.hex(5)
+    referral_code = UsersHelper.generate_referral_id
     collision = User.find_by_referral_code(referral_code)
 
     until collision.nil?
-      referral_code = SecureRandom.hex(5)
+      referral_code = UsersHelper.generate_referral_id
       collision = User.find_by_referral_code(referral_code)
     end
     referral_code
   end
+
+  def generate_referral_id(length=6)
+    o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+    (0...length).map { o[rand(o.length)] }.join
+  end
+  module_function :generate_referral_id
 end
