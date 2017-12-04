5f40bf841d6edac5bb6597ca524b6cd90db1ddb5
André Laszlo <andre@laszlo.nu>
2017-12-04 20:18:22 +0100

    Copy link automatically

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,14 +1,26 @@
-$(document).ready(function(){
-	var myVideo = document.getElementById('video');
-	console.log(myVideo);
-	if(myVideo){
-		if (typeof myVideo.loop == 'boolean') { // loop supported
-		    myVideo.loop = true;
-		} else { // loop property not supported
-		    myVideo.on('ended', function () {
-		    this.currentTime = 0;
-		    this.play();
-		    }, false);
-		}
-	}
-})
+function selectText(text) {
+  var range, selection;
+  if (document.body.createTextRange) {
+    range = document.body.createTextRange();
+    range.moveToElementText(text);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection();
+    range = document.createRange();
+    range.selectNodeContents(text);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+  if (document.execCommand) {
+    try {
+      document.execCommand('copy');
+      $(text).popover('show');
+      setTimeout(function() {
+        $(text).popover('hide');
+      }, 2000);
+    } catch (err) {
+      console.log("Unable to copy", err);
+    }
+  }
+
+}
