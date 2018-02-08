c4fbb534f9e949563b206953d3caa2fcbac84854
André Laszlo <andre@laszlo.nu>
2018-02-08 17:57:36 +0100

    Fix placeholder page

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Prelaunchr
     config.ended = ENV['CAMPAIGN_ENDED'].to_s == 'true'
 
     # decides whether the prelaunch campaign has started or not (show placeholder)
-    config.started = ENV['CAMPAIGN_STARTED'].to_s == 'true'
+    config.started = ENV['CAMPAIGN_STARTED'].to_s == 'false'
 
     if ENV['DEFAULT_HOST']
       config.action_controller.default_url_options = { :host => ENV['DEFAULT_HOST'] }
