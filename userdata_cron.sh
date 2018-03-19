4b9975f0cb1df36a3c8b322107e057ed682c61e7
André Laszlo <andre@laszlo.nu>
2018-03-19 12:15:16 +0100

    Only run userdata cron on first instance

--- userdata_cron.sh
+++ userdata_cron.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env bash
+
+if [ $INSTANCE_NUMBER -ne 0 ]; then
+    echo "Don't run cron on instance $INSTANCE_NUMBER";
+    exit 0;
+fi
+
 source /home/bas/applicationrc
 
 eval "$(/home/bas/.rbenv/bin/rbenv init -)"
